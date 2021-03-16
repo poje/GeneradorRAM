a6fe2ea2cc249f00322e21693efd88fb59e05baf
Jorge Villaseca R <jorge.villaseca91@gmail.com>
2021-03-16 18:26:11 -0300

    Se añaden mensajes con las tareas a realizar

--- GeneradorRAM/GeneradorRAM/Program.cs
+++ GeneradorRAM/GeneradorRAM/Program.cs
@@ -10,7 +10,11 @@ namespace GeneradorRAM
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Obteniendo Fechas de Enviado");
+            Console.WriteLine("Generar Carpetas por Fecha de Envio");
+            Console.WriteLine("Generar Archivos en carpetas de envío");
+            Console.WriteLine("Insertar en base de datos el archivo creado");
+            Console.WriteLine("Insertar en base de datos en tabla de archivos");
         }
     }
 }
