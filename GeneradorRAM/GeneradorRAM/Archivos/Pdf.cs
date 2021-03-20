9193f993f36e33c25df6892c4b590952a4d7405c
Jorge Villaseca R <jorge.villaseca91@gmail.com>
2021-03-20 00:37:17 -0300

    Se agrega Crystal report, además de clases para manejo de lógica de creación de pdf e interfaz de control de comportamientos de esta clase

---
+++ GeneradorRAM/GeneradorRAM/Archivos/Pdf.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeneradorRAM.Interfaces;
+
+namespace GeneradorRAM.Archivos
+{
+    public class Pdf : IArchivo
+    {
+        public Pdf()
+        {
+
+        }
+
+        public List<string> Generar()
+        {
+            var pdfList = new List<string>();
+
+            
+
+            return new List<string>();
+        }
+    }
+}
