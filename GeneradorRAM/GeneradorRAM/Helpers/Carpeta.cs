0f353e7636b012838dcc89425084ed05b3482abe
Jorge Villaseca R <jorge.villaseca91@gmail.com>
2021-03-20 00:18:54 -0300

    Se crea clase encargade crear y eliminar carpetas

---
+++ GeneradorRAM/GeneradorRAM/Helpers/Carpeta.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace GeneradorRAM.Helpers
+{
+    public class Carpeta
+    {
+        public Carpeta()
+        {
+            
+        }
+
+        public void Crear()
+        {
+            if (!Directory.Exists(Properties.Settings.Default.PathRAMTemp))
+            {
+                Directory.CreateDirectory(Properties.Settings.Default.PathRAMTemp);
+            }
+        }
+
+        public void Eliminar()
+        {
+            Directory.Delete(Properties.Settings.Default.PathRAMTemp);
+        }
+    }
+}
