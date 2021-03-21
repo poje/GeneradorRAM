ae3485b4830257d3a222b3d20a7b75c8d31bd6e4
Jorge Villaseca R <jorge.villaseca91@gmail.com>
2021-03-20 22:23:24 -0300

    Se agrega lógica para la generación de archivos PDF según periodo y condiciones de formato de 80 bis

--- GeneradorRAM/GeneradorRAM/Program.cs
+++ GeneradorRAM/GeneradorRAM/Program.cs
@@ -23,7 +23,7 @@ namespace GeneradorRAM
             List<SqlParameter> list = new List<SqlParameter>();
 
             int codProyecto = 1010168;
-            int mesAno = 201901;
+            int mesAno = 202001;
 
             list.Add(new SqlParameter("@CodProyecto", codProyecto));
             list.Add(new SqlParameter("@MesAno", mesAno));
@@ -32,9 +32,62 @@ namespace GeneradorRAM
             var ds = c.EjecutaSpToDataSet("cierre_resumenatencionmensual_Firma_impresion", list);
             var archivo = new Pdf();
 
-            archivo.Generar(Properties.Settings.Default.ResumenAtencionMensualFinal, ds.Tables[0]);
-            archivo.Generar(Properties.Settings.Default.ResumenListadoPlazasConvenidas, ds.Tables[1]);
-            archivo.Generar(Properties.Settings.Default.ResumenListadoNNA80bis, ds.Tables[2]);
+
+            list.Clear();
+            list.Add(new SqlParameter("@CodProyecto", codProyecto));
+            var esCierreNuevo = Convert.ToBoolean(c.EjecutaSpScalar("GetValidacionCierre", list));
+
+            list.Clear();
+            list.Add(new SqlParameter("@CodProyecto", codProyecto));
+            list.Add(new SqlParameter("@Periodo", mesAno));
+            var usaRamNominas = Convert.ToBoolean(c.EjecutaSpScalar("GetUsaRamNueva", list));
+
+            var codModeloIntervencion = Convert.ToInt32(ds.Tables[0].Rows[0]["CodModeloIntervencion"].ToString());
+
+            var pdfs = new List<string>();
+
+            //Generación de Pdfs según condiciones
+            if (esCierreNuevo && usaRamNominas)
+            {
+                pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenAtencionMensualFinal, ds.Tables[0]));
+
+                if (ds.Tables.Count == 2)
+                {
+                    pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenListadoPlazasConvenidas, ds.Tables[1]));
+                }
+
+                if (ds.Tables.Count == 3)
+                {
+                    if (ds.Tables[2].Rows[0]["Listado"].ToString() == "Listado80bis")
+                    {
+                        pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenListadoNNA80bis, ds.Tables[2]));
+                    }
+
+                    if (ds.Tables[2].Rows[0]["Listado"].ToString() == "ListadoSobreAtencion")
+                    {
+                        pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenListadoNNASobreAtencion, ds.Tables[3]));
+
+                    }
+                }
+
+                if (ds.Tables.Count == 4)
+                {
+                    pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenListadoPlazasConvenidas, ds.Tables[1]));
+                    pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenListadoNNA80bis, ds.Tables[2]));
+                    pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenListadoNNASobreAtencion, ds.Tables[3]));
+                }
+            }
+            else
+            {
+                if (codModeloIntervencion == 83)
+                    pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenAtencionMensualPAD, ds.Tables[0]));
+                else if (codModeloIntervencion == 128)
+                    pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenAtencionMensualPJC, ds.Tables[0]));
+                else
+                    pdfs.Add(archivo.Generar(Properties.Settings.Default.ResumenAtencionMensualOriginal, ds.Tables[0]));
+            }
+
+            //Unir Pdf
 
             Console.WriteLine("Obteniendo Fechas de Enviado");
             Console.WriteLine("Generar Carpetas por Fecha de Envio");
@@ -43,7 +96,7 @@ namespace GeneradorRAM
             Console.WriteLine("Insertar en base de datos en tabla de archivos");
 
             Console.WriteLine("Eliminando carpeta temporal");
-            
+
 
 
             Console.ReadKey();
